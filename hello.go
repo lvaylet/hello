b8bfab167462f4480be227533831cdef4f15f27b
Laurent Vaylet <laurent.vaylet@renault.com>
2019-03-01 17:20:10 +0100

    add type declaration

--- hello.go
+++ hello.go
@@ -6,6 +6,12 @@ import (
 	"github.com/user/stringutil"
 )
 
+// T is only used to test gofmt
+type T struct {
+	name  string // name of the object
+	value int    // its value
+}
+
 func main() {
 	fmt.Println(stringutil.Reverse("!oG ,olleH"))
 }
